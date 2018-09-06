a480156d0244cd1ddb03524d8e6d6136595cee1b
Cristian Ospina <ospina_522@hotmail.com>
2018-09-05 23:42:40 -0500

    Main queue

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,8 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { NativeModules } from "react-native";
 
 const { RNVideoPlayer } = NativeModules;
 
 module.exports = {
-  ...RNVideoPlayer,
 
   showVideoPlayer(url) {
     return RNVideoPlayer.showVideoPlayer(url);
